b22dceb203e6462deddc4f1315091a8f66c5ea53
Tanisha <ts144@snu.edu.in>
2019-11-05 15:32:48 +0530

    adding files

---
+++ app/src/main/java/com/microsoft/azure/sdk/iot/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.azure.sdk.iot;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iothub.azure.microsoft.com.androidsample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String ConnectionString = "HostName=iotHubDevMSDN.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=1SpYwGBBfpD7NRl8QChe0ybBKNYajivSFOzYZOQDInY=";
+  public static final String DeviceId = "IotHubDeviceIotWorks";
+}
